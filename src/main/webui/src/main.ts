fb771248bb7f6bfc88ba238f4324c862d0ab67b4
Matthias Kallfass <me@mkallfass.de>
2024-05-16 19:39:55 +0200

    Install primevue/Tailwind - https://tailwind.primevue.org/overview - https://github.com/primefaces/primevue-tailwind - https://codesandbox.io/p/devbox/vue3-primevue-tailwind-repo-dljqpf

--- src/main/webui/src/main.ts
+++ src/main/webui/src/main.ts
@@ -1,14 +1,37 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import PrimeVue from 'primevue/config'
+import Toast from 'primevue/toast';
+import ToastService from 'primevue/toastservice';
+import Menubar from 'primevue/menubar'
+import Card from 'primevue/card'
+import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
+import InputText from 'primevue/inputtext'
+
+import './assets/main.css'
+import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  ripple: true
+});
+app.use(ToastService);
+
+app.component('Button', Button);
+app.component('Toast', Toast);
+app.component('Menubar', Menubar)
+app.component('Card', Card)
+app.component('Dialog', Dialog)
+app.component('Button', Button)
+app.component('InputText', InputText)
 
 app.mount('#app')
--- src/main/webui/src/views/HomeView.vue
+++ src/main/webui/src/views/HomeView.vue
@@ -1,9 +1,30 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <Dialog header="Header" v-model:visible="display" >
+      <Button class="p-button-info p-button-sm" label="Close" @click="display = !display"/>
+    </Dialog>
+
+    <Card class="my-2">
+      <template #title>
+        This is an about page
+      </template>
+      <template #content>
+        <div class="text-sky-500 dark:text-sky-400">
+          Test Text
+        </div>
+      </template>
+      <template #footer>
+        <div class="flex gap-2">
+          <Button label="Open Dialog" @click="display = !display"/>
+          <Button label="Open Dialog" class="p-button-primary" @click="display = !display"/>
+          <Button label="Open Dialog" class="p-button-secondary" @click="display = !display"/>
+        </div>
+      </template>
+    </Card>
   </main>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const display = ref(false)
+</script>
