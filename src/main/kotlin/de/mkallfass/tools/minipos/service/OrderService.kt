b53ddf8a7020598344c2171bb8b3e6beedc67f17
Matthias Kallfass <me@mkallfass.de>
2024-05-26 14:26:54 +0200

    Log diffs order/catalog while processing order

--- src/main/kotlin/de/mkallfass/tools/minipos/service/OrderService.kt
+++ src/main/kotlin/de/mkallfass/tools/minipos/service/OrderService.kt
@@ -36,13 +36,25 @@ class OrderService {
         for (i in order.lineItems) {
             val p = productService.getProductById(i.id)
             if (p != null) {
+                if (i.name != null && i.name != p.name) {
+                    Log.warn("Name in lineitem ${i.name} differs from catalog ${p.name}")
+                }
                 i.name = p.name
+                if (i.description != null && i.description != p.description) {
+                    Log.warn("Description in lineitem ${i.description} differs from catalog ${p.description}")
+                }
                 i.description = p.description
+                if (i.price != null && i.price != p.price) {
+                    Log.warn("Price in lineitem ${i.price} differs from catalog ${p.price}")
+                }
                 i.price = p.price
             } else {
-                throw IllegalArgumentException("LineItem id ${i.id} is not a valid product id")
+                throw IllegalArgumentException("Lineitem id ${i.id} is not a valid product id")
             }
             val lineItemTotal = i.quantity * i.price!!
+            if (i.total != null && i.total != lineItemTotal) {
+                Log.warn("Total in lineitem ${i.total} differs from calculated total ${lineItemTotal}")
+            }
             i.total = lineItemTotal
             orderTotal += lineItemTotal
         }
