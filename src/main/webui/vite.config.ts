ec357614d1205903c13dab1035b4622e318acba6
Matthias Kallfass <me@mkallfass.de>
2024-06-21 00:18:53 +0200

    Fix top-level await, see https://stackoverflow.com/questions/72618944/get-error-to-build-my-project-in-vite-top-level-await-is-not-available-in-the

--- src/main/webui/vite.config.ts
+++ src/main/webui/vite.config.ts
@@ -1,12 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-import VueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext" //browsers can handle the latest ES features
+  },
   plugins: [
     vue(),
     vueJsx(),
