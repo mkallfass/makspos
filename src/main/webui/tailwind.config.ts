86319d193f0205f71a69574f9b0c38b00ac2ea78
Matthias Kallfass <me@mkallfass.de>
2024-11-01 09:37:50 +0100

    Use typescript for tailwind config

---
+++ src/main/webui/tailwind.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'tailwindcss';
+import primeui from 'tailwindcss-primeui';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [primeui],
+} satisfies Config
