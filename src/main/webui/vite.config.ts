42529a636047485da3f731ad0f129c1c43d84236
Matthias Kallfass <me@mkallfass.de>
2025-03-15 11:23:38 +0700

    tailwind 4: Use vite instead of postcss

--- src/main/webui/vite.config.ts
+++ src/main/webui/vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import VueDevTools from "vite-plugin-vue-devtools";
@@ -11,6 +12,7 @@ export default defineConfig({
     target: "esnext" //browsers can handle the latest ES features
   },
   plugins: [
+    tailwindcss(),
     vue(),
     vueJsx(),
     VueDevTools(),
